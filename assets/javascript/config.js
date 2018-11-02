89ff700497537d29ab369af3d6d2e59e8f765a92
Vivien <vchang103@gmail.com>
2018-11-01 20:17:48 -0700

    fix merge conflicts

--- assets/javascript/config.js
+++ assets/javascript/config.js
@@ -1,1 +1,3 @@
-var google_api = "AIzaSyCqbqiyRQlrICB9lp-O0atY_4sGJ1QlAPI"
+var google_key = "AIzaSyCqbqiyRQlrICB9lp-O0atY_4sGJ1QlAPI"
+
+var stormglass = "d3cc5276-dd5b-11e8-9e1f-0242ac130004-d3cc5424-dd5b-11e8-9e1f-0242ac130004"
--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,10 +1,11 @@
-
-// GOOGLE API
+var google_api = google_key;
+var sg_api = stormglass;
 
 $.ajax({
-    url: "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyCqbqiyRQlrICB9lp-O0atY_4sGJ1QlAPI",
+    url: "https://maps.googleapis.com/maps/api/geocode/json?address=5000+Pacific+Coast+Hwy,+Pacifica,+CA&key=" + google_api,
     method: "GET",
 }).then(function(response) {
+	console.log(response)
 	console.log(response.results[0].geometry.location.lat)
 	console.log(response.results[0].geometry.location.lng)
 })
@@ -24,20 +25,21 @@ var lon = "";
     $("#lon").on("click", function() {
         lon = prompt("longitude:")
     });
-    
-        $("#btn1").on("click", function() {
+
+    $("#btn1").on("click", function() {
 
 
-$.ajax({
-    //url: 'https://api.stormglass.io/point?lat=58.5&lng=17.8',
-    url: "https://api.stormglass.io/point?lat=" + lat + "&lng=" + lon,
-    method: "GET",
-    headers: { 'Authorization': "d3cc5276-dd5b-11e8-9e1f-0242ac130004-d3cc5424-dd5b-11e8-9e1f-0242ac130004" }
-}).then(function(response) {
-	console.log(response)
-    console.log(response.hours)
-    
-    });
-});
+		$.ajax({
+		    //url: 'https://api.stormglass.io/point?lat=58.5&lng=17.8',
+		    url: "https://api.stormglass.io/point?lat=" + lat + "&lng=" + lon,
+		    method: "GET",
+		    headers: { 'Authorization': sg_api }
+		}).then(function(response) {
+			console.log(response)
+		  console.log(response.hours)
+			console.log(response.hours[4])
+			console.log(response.hours[4].swellHeight)
 
+		});
+	});
 });
