af3c72694d6bc64e060b83e3ea28bab00bccd579
Vivien <vchang103@gmail.com>
2018-11-08 18:06:49 -0800

    move into functions and refactor code

--- assets/javascript/config.js
+++ assets/javascript/config.js
@@ -1,4 +1,4 @@
 var google_key = "AIzaSyCqbqiyRQlrICB9lp-O0atY_4sGJ1QlAPI"
-var backup_stormglass = "dd5c9c48-de4f-11e8-9f7a-0242ac130004-dd5c9d4c-de4f-11e8-9f7a-0242ac130004"
-var backup_sg_3 = "3163bc06-dfac-11e8-83ef-0242ac130004-3163bd00-dfac-11e8-83ef-0242ac130004"
 var stormglass = "d3cc5276-dd5b-11e8-9e1f-0242ac130004-d3cc5424-dd5b-11e8-9e1f-0242ac130004"
+var stormglassTwo = "dd5c9c48-de4f-11e8-9f7a-0242ac130004-dd5c9d4c-de4f-11e8-9f7a-0242ac130004"
+var stormglassThree = "3163bc06-dfac-11e8-83ef-0242ac130004-3163bd00-dfac-11e8-83ef-0242ac130004"
